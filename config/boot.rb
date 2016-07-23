6349eba0275ee6509869c4cebebcfd7a1a5b0170
Hamza Ghani <h.u.f.ghani@gmail.com>
2016-07-23 23:39:14 +0100

    set up and config of server

---
+++ config/boot.rb
@@ -0,0 +1,13 @@
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+
+require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+require 'rails/commands/server'
+
+module Rails
+  class Server
+    def default_options
+      super.merge({Host:  '192.168.0.19', Port: 6562})
+    end
+  end
+end
